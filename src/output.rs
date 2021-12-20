66235af45826e7635816df3a2a1d0c43e7cdda0f
Roman Gushchin <guro@fb.com>
2021-12-20 11:19:25 -0800

    mole: enhance output lib
    Signed-off-by: Roman Gushchin <guro@fb.com>

--- src/output.rs
+++ src/output.rs
@@ -1,3 +1,4 @@
+use std::cmp::Ordering;
 pub enum Data {
     UInt(u64),
     Int(i64),
@@ -17,11 +18,6 @@ pub struct Table {
     pub filter_by: Option<usize>,
 }
 
-pub fn add_row(table: &mut Table, data: Vec<Data>) {
-    assert_eq!(table.columns.len(), data.len());
-    table.data.push(data);
-}
-
 fn default_fmt(data: &Data, column: &Column) -> String {
     match data {
         Data::UInt(v) => format!("{1:<0$}", column.width, v),
@@ -31,50 +27,56 @@ fn default_fmt(data: &Data, column: &Column) -> String {
     }
 }
 
-fn compare_data(a: &Data, b: &Data) -> std::cmp::Ordering {
+fn compare_data(a: &Data, b: &Data) -> Ordering {
     match (a, b) {
         (Data::UInt(x), Data::UInt(y)) => x.cmp(&y),
         (Data::Int(x), Data::Int(y)) => x.cmp(&y),
-        (Data::Float(x), Data::Float(y)) => x.partial_cmp(&y).unwrap(),
+        (Data::Float(x), Data::Float(y)) => x.partial_cmp(&y).unwrap_or(Ordering::Less),
         (Data::Text(x), Data::Text(y)) => x.cmp(&y),
         (_, _) => panic!(),
     }
 }
 
-pub fn display_table(table: &mut Table) -> String {
-    let mut output = String::new();
-    let delimiter = String::from(" ");
-    let newline = String::from("\n");
-
-    // sort data
-    if table.sort_by.is_some() {
-        let sort_by = table.sort_by.unwrap();
-        table
-            .data
-            .sort_unstable_by(|a, b| compare_data(&a[sort_by], &b[sort_by]));
+impl Table {
+    pub fn add_row(&mut self, data: Vec<Data>) {
+        assert_eq!(self.columns.len(), data.len());
+        self.data.push(data);
     }
 
-    // print titles
-    for column in &table.columns {
-        output.push_str(&format!("{:width$}", column.title, width = column.width));
-        output.push_str(&delimiter);
-    }
-    output.push_str(&newline);
-    output.push_str(&"-".repeat(output.len() - 2));
-    output.push_str(&newline);
+    pub fn display_table(&mut self) -> String {
+        let mut output = String::new();
+        let delimiter = String::from(" ");
+        let newline = String::from("\n");
 
-    // print data
-    for row in &table.data {
-        let mut i = 0;
-        for cell in row {
-            output.push_str(&default_fmt(cell, &table.columns[i]));
+        // sort data
+        if self.sort_by.is_some() {
+            let sort_by = self.sort_by.unwrap();
+            self.data
+                .sort_unstable_by(|a, b| compare_data(&a[sort_by], &b[sort_by]));
+        }
+
+        // print titles
+        for column in &self.columns {
+            output.push_str(&format!("{:width$}", column.title, width = column.width));
             output.push_str(&delimiter);
-            i += 1;
         }
         output.push_str(&newline);
-    }
+        output.push_str(&"-".repeat(output.len() - 2));
+        output.push_str(&newline);
 
-    output
+        // print data
+        for row in &self.data {
+            let mut i = 0;
+            for cell in row {
+                output.push_str(&default_fmt(cell, &self.columns[i]));
+                output.push_str(&delimiter);
+                i += 1;
+            }
+            output.push_str(&newline);
+        }
+
+        output
+    }
 }
 
 #[macro_export]
@@ -93,42 +95,30 @@ macro_rules! table {
 fn create_test_table() {
     let mut t = table![("Pid", 8), ("Comm", 16), ("usr%", 4), ("Sys%", 4)];
 
-    add_row(
-        &mut t,
-        vec![
-            Data::Int(1),
-            Data::Text("aaa".to_string()),
-            Data::Float(0.1),
-            Data::Float(0.01),
-        ],
-    );
-    add_row(
-        &mut t,
-        vec![
-            Data::Int(2),
-            Data::Text("bbb".to_string()),
-            Data::Float(0.2),
-            Data::Float(0.0),
-        ],
-    );
-    add_row(
-        &mut t,
-        vec![
-            Data::Int(3),
-            Data::Text("ccc".to_string()),
-            Data::Float(0.0),
-            Data::Float(0.04),
-        ],
-    );
-    add_row(
-        &mut t,
-        vec![
-            Data::Int(4),
-            Data::Text("ddd".to_string()),
-            Data::Float(0.0),
-            Data::Float(0.05),
-        ],
-    );
+    t.add_row(vec![
+        Data::Int(1),
+        Data::Text("aaa".to_string()),
+        Data::Float(0.1),
+        Data::Float(0.01),
+    ]);
+    t.add_row(vec![
+        Data::Int(2),
+        Data::Text("bbb".to_string()),
+        Data::Float(0.2),
+        Data::Float(0.0),
+    ]);
+    t.add_row(vec![
+        Data::Int(3),
+        Data::Text("ccc".to_string()),
+        Data::Float(0.0),
+        Data::Float(0.04),
+    ]);
+    t.add_row(vec![
+        Data::Int(4),
+        Data::Text("ddd".to_string()),
+        Data::Float(0.0),
+        Data::Float(0.05),
+    ]);
 
-    println!("{}", display_table(&mut t));
+    println!("{}", t.display_table());
 }
